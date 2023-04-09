b0e0c14088e40d75b1c9a2841ab032bcf83a0f60
mva919 <mva919@gmail.com>
2023-04-09 12:38:19 -0700

    Replace user button with custom sign out button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,21 @@
-import { UserButton } from "@clerk/nextjs";
+import { useClerk } from "@clerk/nextjs";
 import ThemeToggle from "./themetoggle";
+import { LogOut } from "lucide-react";
 
 const Navbar = () => {
+  const { signOut } = useClerk();
+
   return (
     <nav className="flex h-14 w-full items-center justify-between border-b border-slate-200 px-4 dark:border-slate-600 ">
-      <h1 className="font-semibold dark:text-slate-100">Remind me</h1>
-      <div className="flex items-center justify-between gap-12">
+      <h1 className="font-semibold dark:text-slate-100">Remind me.</h1>
+      <div className="flex items-center justify-between gap-6">
         <ThemeToggle />
-        <UserButton />
+        <button
+          className="rounded bg-red-600 p-2 text-white transition-all duration-100 hover:scale-95 hover:bg-red-700"
+          onClick={() => void signOut()}
+        >
+          <LogOut height={18} width={18} />
+        </button>
       </div>
     </nav>
   );
