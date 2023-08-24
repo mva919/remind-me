b1555034cc1cac5aab2845e2ff24f9605adc9c0d
mva919 <mva919@gmail.com>
2023-08-24 12:07:11 -0700

    Add height to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex w-full flex-none items-center justify-center gap-4 border-t border-slate-300 py-2 dark:border-slate-600">
+    <footer className="flex h-14 w-full flex-none items-center justify-center gap-4 border-t border-slate-300 py-2 dark:border-slate-600">
       <Link href="https://github.com/mva919/remind-me">
         <Github />
       </Link>
