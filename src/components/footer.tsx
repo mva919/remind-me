972419f33832727038e9f94a70409431551945e1
mva919 <mva919@gmail.com>
2023-08-24 17:57:12 -0700

    Add Button to github icon link in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,15 @@
 import { Github } from "lucide-react";
 import Link from "next/link";
+import Button from "./button";
 
 const Footer = () => {
   return (
     <footer className="flex h-14 w-full flex-none items-center justify-center gap-4 border-t border-slate-300 py-2 dark:border-slate-600">
-      <Link href="https://github.com/mva919/remind-me">
-        <Github />
-      </Link>
+      <Button>
+        <Link href="https://github.com/mva919/remind-me">
+          <Github />
+        </Link>
+      </Button>
       <p>Created by Marcos Villanueva Abreu</p>
     </footer>
   );
